8814ff1739f7535b2388e7694421cecb09080739
Muhamadfajaryh12 <2010631170090@student.unsika.ac.id>
2024-07-02 10:59:29 +0700

    database-access

---
+++ database-access/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+    // Capture connection properties.
+		dbUser := "fajar-go"
+	dbPass := "fajarbaru789"
+    cfg := mysql.Config{
+		User:   dbUser,
+		Passwd: dbPass,
+        Net:    "tcp",
+        Addr:   "127.0.0.1:3306",
+        DBName: "recordings",
+    }
+    // Get a database handle.
+    var err error
+    db, err = sql.Open("mysql", cfg.FormatDSN())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    pingErr := db.Ping()
+    if pingErr != nil {
+        log.Fatal(pingErr)
+    }
+    fmt.Println("Connected!")
+}
---
+++ learn_api_go/go.mod
@@ -0,0 +1,3 @@
+module learn_api
+
+go 1.22.2
