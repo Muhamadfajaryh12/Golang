083536e063b3c907b73969a522033b40fa7693c3
Muhamadfajaryh12 <2010631170090@student.unsika.ac.id>
2024-07-02 11:34:20 +0700

    fix bug connection db

--- database-access/main.go
+++ database-access/main.go
@@ -4,33 +4,45 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var db *sql.DB
 
 func main() {
-    // Capture connection properties.
-		dbUser := "fajar-go"
-	dbPass := "fajarbaru789"
-    cfg := mysql.Config{
-		User:   dbUser,
-		Passwd: dbPass,
-        Net:    "tcp",
-        Addr:   "127.0.0.1:3306",
-        DBName: "recordings",
-    }
-    // Get a database handle.
-    var err error
-    db, err = sql.Open("mysql", cfg.FormatDSN())
+    // Load environment variables from .env file
+    err := godotenv.Load(".env")
     if err != nil {
-        log.Fatal(err)
+        log.Fatalf("Error loading .env file: %v", err)
     }
 
+    // Get environment variables
+    dbUser := os.Getenv("DBUSER")
+    dbPass := os.Getenv("DBPASS")
+
+    // Check if DBUSER and DBPASS are not empty
+    if dbUser == "" || dbPass == "" {
+        log.Fatal("DBUSER or DBPASS environment variable is not set")
+    }
+
+    // MySQL connection configuration
+    dataSourceName := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/recordings", dbUser, dbPass)
+
+    // Open database connection
+    var errDB error
+    db, errDB = sql.Open("mysql", dataSourceName)
+    if errDB != nil {
+        log.Fatal(errDB)
+    }
+
+    // Ping database to check connection
     pingErr := db.Ping()
     if pingErr != nil {
         log.Fatal(pingErr)
     }
-    fmt.Println("Connected!")
+
+    fmt.Println("Connected to MySQL database!")
 }
